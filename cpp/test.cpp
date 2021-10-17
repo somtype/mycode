f81b944d00e84c8da4991f9adafea43be4bfdb63
SomType <somtype@icloud.com>
2021-10-17 21:25:50 +0800

    日常

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,13 +3,6 @@
 #include <map>
 using namespace std;
 int main() {
-    set<int> s1;
-    s1.insert(1);
-    s1.insert(3);
-    s1.insert(5);
-    map<string,int> m1;
-    /*
-    asdds
-    */
-    return 0;
+    int a = !(1 > 2);
+    cout << a << endl;
 }
