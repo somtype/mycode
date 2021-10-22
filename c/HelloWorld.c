4e43a91321714746387ecb404a97b72e9ee66138
SomType <somtype@icloud.com>
2021-10-23 01:07:14 +0800

    日常

--- c/HelloWorld.c
+++ c/HelloWorld.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-unsigned int reverse(unsigned int x) {
-  unsigned int b = 0, c;
-  while (x != 0) {
-    c = x % 10;
-    b = b * 10 + c;
-    x /= 10;
-  }
-  return b;
-}
-int detectBits(int x) {
-  return !(x & 2863311530 ^ 2863311530);
-}
 int main() {
-  unsigned int result = reverse(108);
-  printf("%u\n", result);
-  return 0;
+    printf("Hello\n");
 }
