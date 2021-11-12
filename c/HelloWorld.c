61ce47391e3509928ba71cfec77cc279c6d3c85e
SomType <somtype@icloud.com>
2021-11-13 00:40:03 +0800

    日常

--- c/HelloWorld.c
+++ c/HelloWorld.c
@@ -1,4 +1,2 @@
 #include <stdio.h>
-int main() {
-    printf("Hello\n");
-}
+int main() { printf("Hello\n"); }
--- c/test.c
+++ c/test.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main() {
-  int n = 100;
-  scanf("%d", &n);
-  printf("%d\n", detectBits(n));
-  return 0;
+    int n = 100;
+    scanf("%d", &n);
+    printf("%d\n", detectBits(n));
+    return 0;
 }
-int detectBits(int x) {
-  return !(x & 2863311530 ^ 2863311530);
-}
+int detectBits(int x) { return !(x & 2863311530 ^ 2863311530); }
