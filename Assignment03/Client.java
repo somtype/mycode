a74205153391ebaa18488f986acdce2366b9faaa
SomType <somtype@icloud.com>
2021-12-01 21:37:32 +0800

    日常

--- Assignment03/Client.java
+++ Assignment03/Client.java
@@ -4,21 +4,22 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Client implements Runnable{
+public class Client{
     //输入参数表
     static InetAddress server; //服务器
     static String servername = "localhost";
     static int serverPort; //服务器的端口
     static int threadsNum; //能用的线程数
-    static List<String> filenames; //需要的文件列表
+    static String[] fileNames; //需要的文件列表
     static long time2wait = 5; //运行时间
     //需要用到的变量
     private ClientThread[] threads; //线程池
-    public Client(){
+    static List<String> fileList; //待处理的文件列表
+    public Client() throws Exception{
         //生成线程池，每个线程维持一个TCP连接
         threads = new ClientThread[threadsNum];
         for(int i = 0; i < threadsNum; i++){
-            threads[i] = new ClientThread(filenames);
+            threads[i] = new ClientThread(server, serverPort, fileList);
             new Thread(threads[i]).run();
         }
     }
@@ -32,9 +33,9 @@ public class Client implements Runnable{
         if (args.length >= 3) {
             threadsNum = Integer.parseInt(args[2]);
         }
-        filenames = new ArrayList<String>();
+        fileNames = new String[];
         for (int i = 0; i < threadsNum; i++) {
-            filenames.add(args[3 + i]);
+            fileList.add(args[3 + i]);
         }
         if (args.length >= 3 + threadsNum + 1) {
             time2wait = Long.parseLong(args[3 + threadsNum]);
@@ -43,7 +44,7 @@ public class Client implements Runnable{
         timer.schedule(new TimerTask(){
             @Override
             public void run(){
-                System.out.println("吞吐量: " + WebRequestMaker.totalBytes/time2wait + "Bps");
+                System.out.println("吞吐量: " + ClientThread.totalBytes/time2wait + "Bps");
                 System.exit(1);
             }
         },1000 * time2wait);
--- Assignment03/ClientThread.java
+++ Assignment03/ClientThread.java
@@ -3,44 +3,57 @@ import java.util.*;
 import java.net.*;
 
 public class ClientThread implements Runnable {
-    private List<String> fileList;
+    private List<String> fileList = null;
+    private String filename = null;
     Socket socket = null;
     static long totalBytes = 0;
-    public ClientThread(InetAddress server, int port, List<String> fileList) throws IOException{
-        this.socket = new Socket(server,port);
+
+    public ClientThread(InetAddress server, int port, List<String> fileList) throws Exception{
+        this.socket = new Socket(server, port);
         this.fileList = fileList;
     }
+
     @Override
-    public void run() {
-        try {
-            
-        } catch (IOException e) {
+    public void run(){
+        while (true) {
+            synchronized (fileList) {
+                while (fileList.isEmpty()) {
+                    try {
+                        fileList.wait();
+                    } catch (InterruptedException e) {
+                    }
+                }
+                filename = fileList.remove(0);
+            }
+            try {
+                MakeRequest();
+            } catch (Exception e) {
+            }
         }
     }
-    void makeRequest(){
-        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-            outToServer.writeBytes("GET " + filename + " HTTP/1.0\r\n");
-            outToServer.writeBytes("Host: " + clientSocket.getLocalAddress() + "\r\n");
-            outToServer.writeBytes("\r\n");
 
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String line = inFromServer.readLine();
-            int code = Integer.parseInt(line.split("\\s")[1]);  // Get response code
-            if(code == 200){
-                inFromServer.readLine();    // Content-Type
-                inFromServer.readLine();    // Set-Cookie
-                line = inFromServer.readLine(); // Content-Length
-                int currBytes = Integer.parseInt(line.split("\\s")[1]);
-                for(int i = 0; i < currBytes; i++){
-                    inFromServer.read();
-                }
-                synchronized (this.getClass()){
-                    totalBytes += currBytes;
-                }
+    void MakeRequest() throws Exception{
+        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+        outToServer.writeBytes("GET " + filename + " HTTP/1.0\r\n");
+        outToServer.writeBytes("Host: " + socket.getLocalAddress() + "\r\n");
+        outToServer.writeBytes("\r\n");
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line = inFromServer.readLine();
+        int code = Integer.parseInt(line.split("\\s")[1]); //Get response code
+        if (code == 200) {
+            inFromServer.readLine(); //Content-Type
+            inFromServer.readLine(); //Set-Cookie
+            line = inFromServer.readLine(); //Content-Length
+            int currBytes = Integer.parseInt(line.split("\\s")[1]);
+            for (int i = 0; i < currBytes; i++) {
+                inFromServer.read();
             }
-            else{
-                return;
+            synchronized (ClientThread.class) {
+                totalBytes += currBytes;
             }
+        } else {
+            return;
+        }
     }
 }
-
