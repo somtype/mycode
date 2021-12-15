9b0d98aa6a606416f7e11b66e5472828418aa6ba
SomType <somtype@icloud.com>
2021-12-15 23:42:47 +0800

    日常

--- leetcode/95.不同的二叉搜索树-ii.java
+++ leetcode/95.不同的二叉搜索树-ii.java
@@ -1,3 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=95 lang=java
+ *
+ * [95] 不同的二叉搜索树 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
         if (n == 0)
@@ -26,3 +48,4 @@ class Solution {
         return allTrees;
     }
 }
+// @lc code=end
