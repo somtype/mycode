c16634f931e5313276abf448e6d257a8f1a9fea8
SomType <somtype@icloud.com>
2022-01-01 21:14:40 +0800

    leetcode

---
+++ leetcode/117.填充每个节点的下一个右侧节点指针-ii.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=java
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            for (int i = 1; i <= levelNum; ++i) {
+                Node node = queue.poll();
+                if (i < levelNum)
+                    node.next = queue.peek();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
